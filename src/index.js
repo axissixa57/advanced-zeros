1b5af2f524338881e53b601e5be786c277b25491
Ax1S <axissixa@mail.ru>
2019-03-14 11:47:21 +0300

    feat: add solution to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    let simpleNumbers = {};
+    let i = 2;
+    let res = 0;
+
+    for ( ; i <= base; i++) {
+        while (base % i == 0) {
+            simpleNumbers[i] = simpleNumbers[i] ? simpleNumbers[i] + 1 : 1;
+            base /= i;
+        }
+    }
+    
+    for (key in simpleNumbers) {
+        let tempNum = number;
+        let tempRes = 0;
+
+        while (tempNum > 0) {
+            tempNum = Math.floor(tempNum / key);
+            tempRes += tempNum;
+        }
+
+        if (res == 0 || res > Math.floor(tempRes / simpleNumbers[key]))
+            res = Math.floor(tempRes / simpleNumbers[key]);
+    }
+
+    return res;
 }
